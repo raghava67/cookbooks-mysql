8cb604d5170884fc3ab67a1e981519b44c84c9e1
raghava67 <37594787+raghava67@users.noreply.github.com>
2018-03-20 19:12:14 -0400

    Update mysql_community.rb

--- attributes/mysql_community.rb
+++ attributes/mysql_community.rb
@@ -1,19 +1,4 @@
-efault['yum']['mysql55-community']['repositoryid'] = 'mysql56-community'
-default['yum']['mysql55-community']['gpgkey'] = 'http://repo.mysql.com/RPM-GPG-KEY-mysql'
-default['yum']['mysql55-community']['description'] = 'MySQL 5.6 Community Server'
-default['yum']['mysql55-community']['failovermethod'] = 'priority'
-default['yum']['mysql55-community']['gpgcheck'] = true
-default['yum']['mysql55-community']['enabled'] = true
+default['mysql01']['web']['document_root'] = '/var/www/default/public_html'
 
-case node['platform_family']
-
-when 'amazon'
-  default['yum']['mysql55-community']['baseurl'] = case node['platform_version'].to_i
-                                                   when /201./
-                                                     'http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/'
-                                                   when 2
-                                                     'http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/'
-                                                   else
-                                                     'http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/'
-                                                   end
-end
+default['mysql01']['database']['dbname'] = 'illuminate'
+default['mysql01']['database']['admin_username'] = 'illuminate'
